79ee9d9e4a362ef24c374fa5bde857add32c7d8f
RostyslavKru <r0st1kkr86@gmail.com>
2019-03-26 19:51:07 +0200

    Init commit

--- src/com/company/home_work/HW_01_2.java
+++ src/com/company/home_work/HW_01_2.java
@@ -5,7 +5,7 @@ public class HW_01_2 {
         int i=205;
         byte b=30;
         char c='r';
-        boolean bool=true;
+        boolean bl=true;
         short s=351;
         float f=35.3f;
         double d=74.3;
@@ -15,7 +15,7 @@ public class HW_01_2 {
         System.out.println(i);
         System.out.println(b);
         System.out.println(c);
-        System.out.println(bool);
+        System.out.println(bl);
         System.out.println(s);
         System.out.println(f);
         System.out.println(d);
