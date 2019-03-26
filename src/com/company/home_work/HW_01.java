c8b59b25008bd5f3ab8a0ec15311d73623d8a1b8
RostyslavKru <r0st1kkr86@gmail.com>
2019-03-26 19:41:54 +0200

    Init commit

--- src/com/company/home_work/HW_01.java
+++ src/com/company/home_work/HW_01.java
@@ -1,9 +1,24 @@
 package com.company.home_work;
 
-import com.company.Main;
+
 
 public class HW_01 {
     public static void main(String[] args) {
+int number1=10;
+int number2=18;
+int number3=21;
+int number4=number1;
+
+number1=number2;
+number2=number3;
+number3=number4;
+
+
+System.out.println(number1);
+System.out.println(number2);
+System.out.println(number3);
+
+
 
     }
 }
--- src/com/company/lesson_02/Test_01.java
+++ src/com/company/lesson_02/Test_01.java
@@ -2,5 +2,7 @@ package com.company.lesson_02;
 
 
 public class Test_01 {
+public static void main(String[] args){
 
+}
 }
