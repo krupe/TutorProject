4066a30aca47641c7d82b805c74abd2adaa12fb4
RostyslavKru <r0st1kkr86@gmail.com>
2019-03-26 19:44:41 +0200

    Init commit

---
+++ src/com/company/home_work/HW_01_2.java
@@ -0,0 +1,27 @@
+package com.company.home_work;
+
+public class HW_01_2 {
+    public static void main(String[] args){
+        int i=205;
+        byte b=30;
+        char c='r';
+        boolean bool=true;
+        short s=351;
+        float f=35.3f;
+        double d=74.3;
+        long l=2378653156L;
+        String str="GH";
+
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(bool);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(l);
+        System.out.println(str);
+
+
+    }
+}
---
+++ src/com/company/home_work/HW_01_3.java
@@ -0,0 +1,11 @@
+package com.company.home_work;
+
+public class HW_01_3 {
+    public static void main (String[] args){
+        int a=50;
+        System.out.println("Я буду зарабатывать $"+a+" в час.");
+        System.out.format("Я буду зарабатывать $%d в час.",a);
+
+
+    }
+}
