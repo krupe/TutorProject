8b01e4e509255de90a244de2a859c17985811c96
krupe <r0st1kkr86@gmail.com>
2019-03-28 14:37:46 +0200

    Delete HW_06.java

--- src/com/company/home_work/home_work_04/HW_06.java
+++
@@ -1,9 +0,0 @@
-package com.company.home_work.home_work_04;
-/*
-Ввести с клавиатуры три числа, вывести на экран среднее из них.
-*/
-public class HW_06 {
-    public static void main (String[] args){
-
-    }
-}
