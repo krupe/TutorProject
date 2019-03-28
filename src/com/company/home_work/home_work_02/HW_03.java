83b455c69982ff32b7a068d5cd12a0b7dc89942a
RostyslavKru <r0st1kkr86@gmail.com>
2019-03-28 13:06:50 +0200

    Init commit

--- src/com/company/home_work/home_work_02/HW_03.java
+++ src/com/company/home_work/home_work_02/HW_03.java
@@ -5,5 +5,24 @@ package com.company.home_work.home_work_02;
 Коля получает 3000 через 5 лет.
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class HW_03 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Name:");
+        String str = bf.readLine();
+        System.out.println("Ваше число");
+        String str2=bf.readLine();
+        int number=Integer.parseInt(str2);
+        System.out.println("Enter:");
+        int number2=Integer.parseInt(bf.readLine());
+        System.out.println(str+" получает "+number+" через "+number2+" лет.");
+
+    }
+
+
 }
