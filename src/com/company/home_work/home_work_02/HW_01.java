9990cd21f26dce06fb8bac00c223e8b5b232c265
RostyslavKru <r0st1kkr86@gmail.com>
2019-03-28 14:23:07 +0200

    Init commit

--- src/com/company/home_work/home_work_02/HW_01.java
+++ src/com/company/home_work/home_work_02/HW_01.java
@@ -3,5 +3,23 @@ package com.company.home_work.home_work_02;
     Ввести с клавиатуры три числа, вывести на экран максимальное из них.
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HW_01 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter number1:");
+        int num1 =Integer.parseInt(bf.readLine());
+        System.out.println("Enter number2:");
+        int num2 =Integer.parseInt(bf.readLine());
+
+        if(num1>num2){
+            System.out.println(num2);
+        } else{
+            System.out.println(num1);
+        }
+
+    }
 }
--- src/com/company/home_work/home_work_04/HW_06.java
+++ src/com/company/home_work/home_work_04/HW_06.java
@@ -3,4 +3,7 @@ package com.company.home_work.home_work_04;
 Ввести с клавиатуры три числа, вывести на экран среднее из них.
 */
 public class HW_06 {
+    public static void main (String[] args){
+
+    }
 }
