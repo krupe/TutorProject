150592b08988679dc1586a328461640531de418d
RostyslavKru <r0st1kkr86@gmail.com>
2019-04-02 10:32:55 +0300

    Init commit

--- src/com/company/home_work/home_work_05/HW_05.java
+++ src/com/company/home_work/home_work_05/HW_05.java
@@ -1,4 +1,9 @@
 package com.company.home_work.home_work_05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
 Ввести с клавиатуры имя и используя цикл for 10 раз вывести: [*имя* программист.]
 Пример текста:
@@ -6,4 +11,16 @@ package com.company.home_work.home_work_05;
 …
 */
 public class HW_05 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter name");
+        String st= bf.readLine();
+
+
+        for(int i=1; i <= 10; i++){
+            System.out.println(st+" programmer.");
+        }
+    }
+
+
 }
