103198c31dbdfd79e59aceabdb87530a1da79908
RostyslavKru <r0st1kkr86@gmail.com>
2019-04-02 15:20:46 +0300

    Init commit

--- src/com/company/home_work/home_work_05/HW_03.java
+++ src/com/company/home_work/home_work_05/HW_03.java
@@ -5,4 +5,12 @@ package com.company.home_work.home_work_05;
 */
 
 public class HW_03 {
+    public static void main(String[] args) {
+
+        int r = 1;
+        while (r < 11) {
+            System.out.println("ssssssssss");
+            r++;
+        }
+    }
 }
