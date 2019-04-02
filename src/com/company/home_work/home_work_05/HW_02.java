655d5e1b3e56ae03df6751e14e9171c02eee2820
RostyslavKru <r0st1kkr86@gmail.com>
2019-04-02 15:21:14 +0300

    Init commit

--- src/com/company/home_work/home_work_05/HW_02.java
+++ src/com/company/home_work/home_work_05/HW_02.java
@@ -1,4 +1,9 @@
 package com.company.home_work.home_work_05;
+
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+
 /*
 Ввести с клавиатуры два числа m и n.
 Используя цикл for вывести на экран прямоугольник размером m на n из восьмёрок.
@@ -7,4 +12,16 @@ package com.company.home_work.home_work_05;
 8888
 */
 public class HW_02 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Ввести число1:");
+        int num1=Integer.parseInt(bf.readLine());
+        System.out.println("Ввести число2:");
+        int num2=Integer.parseInt(bf.readLine());
+
+        for(int i=1; i<=2;i++){
+            System.out.println("8888");
+        }
+    }
 }
