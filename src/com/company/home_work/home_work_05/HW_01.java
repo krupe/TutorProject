7ff334ee40a8bb15e69552402d3a8555bbd6a803
RostyslavKru <r0st1kkr86@gmail.com>
2019-04-02 15:20:17 +0300

    Init commit

--- src/com/company/home_work/home_work_05/HW_01.java
+++ src/com/company/home_work/home_work_05/HW_01.java
@@ -10,5 +10,25 @@ package com.company.home_work.home_work_05;
 абв
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HW_01 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Ввести строку:");
+        String st = bf.readLine();
+        System.out.println("Ввести число:");
+        int num = Integer.parseInt(bf.readLine());
+
+        int r = 1;
+        while (r < 3) {
+            System.out.println(st);
+            r++;
+        }
+
+
+    }
 }
