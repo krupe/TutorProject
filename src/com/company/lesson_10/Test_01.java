5ccce76669b28bd1b247abae0a6a130543ca9d81
RostyslavKru <r0st1kkr86@gmail.com>
2019-04-11 21:43:43 +0300

    Init commit

---
+++ src/com/company/lesson_10/Test_01.java
@@ -0,0 +1,44 @@
+package com.company.lesson_10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+1. В методе initializeArray():
+1.1. Создайте массив на 10 чисел
+1.2. Считайте с консоли 10 чисел и заполните ими массив
+2. Метод max(int[] array) должен находить максимальное число из элементов массива
+3. В выполняющем методе main выведите максимальное число в консоль
+*/
+public class Test_01 {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        initializeArray();
+        max();
+
+        System.out.println();
+
+
+    }
+
+    public static void initializeArray(int[] array) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < array.length; i++) {
+
+            array[i] = Integer.parseInt(bf.readLine());
+        }
+
+    }
+
+    public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
+
