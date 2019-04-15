5907667070224e0f59afb9b777e2d779f756c9b7
RostyslavKru <r0st1kkr86@gmail.com>
2019-04-15 15:01:17 +0300

    Init commit

--- src/com/company/home_work/home_work_04/HW_01.java
+++ src/com/company/home_work/home_work_04/HW_01.java
@@ -4,12 +4,31 @@ package com.company.home_work.home_work_04;
 // если параметр метода меньше 5 - и, выводить строку "Число больше 5", если параметр
 // метода больше 5 - и, и выводить строку "Число равно 5", если параметр метода равен 5 - и.
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HW_01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int a=input();
+        compare(a);
 
     }
+    public static int input() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(bf.readLine());
+    }
 
+public static void compare(int a){
+        if(a<5){
+            System.out.print("Число меньше 5");
+        }else if(a>5){
+            System.out.print("Число больше 5");
+        }else if(a==5){
+            System.out.print("Число равно 5");
+        }
 
+}
 
 
 
--- src/com/company/home_work/home_work_04/HW_02.java
+++ src/com/company/home_work/home_work_04/HW_02.java
@@ -2,8 +2,22 @@ package com.company.home_work.home_work_04;
 /*Ввести с клавиатуры число. Если число положительное, то увеличить его в два раза.
  Если число отрицательное, то прибавить единицу. Вывести результат на экран. */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HW_02 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter number:");
+        int num=Integer.parseInt(bf.readLine());
+        if(num>0){
+            num=num*2;
+            System.out.println(num);
+        }else if(num<0){
+            num=num+1;
+            System.out.println(num);
+        }
 
     }
 }
